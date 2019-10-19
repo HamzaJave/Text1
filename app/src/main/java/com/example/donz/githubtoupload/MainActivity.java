2821f89d1e70aa0007e4e68cd0095d40767cea32
Hamza Jvd <03224890Hamza@gmail.com>
2019-10-19 12:53:30 +0500

    New With Toast1

--- app/src/main/java/com/example/donz/githubtoupload/MainActivity.java
+++ app/src/main/java/com/example/donz/githubtoupload/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Hamza Github Test", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hamza Github Test2", Toast.LENGTH_SHORT).show();
     }
 }
