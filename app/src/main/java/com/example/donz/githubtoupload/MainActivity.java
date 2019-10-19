49e1981b9de01d0afb4ab7e0cdbeb388eede6ddf
Hamza Jvd <03224890Hamza@gmail.com>
2019-10-19 13:54:44 +0500

    Hamza1

--- app/src/main/java/com/example/donz/githubtoupload/MainActivity.java
+++ app/src/main/java/com/example/donz/githubtoupload/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Waleed ko new file bhej rha hun
+        // Waleed new things reveived
     }
 }
