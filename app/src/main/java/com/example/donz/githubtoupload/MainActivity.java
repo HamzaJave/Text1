d3681af682cf57dd1d51046c95f49f47f7ae97f7
Hamza Jvd <03224890hamza@gmail.com>
2019-10-19 13:05:45 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/donz/githubtoupload/MainActivity.java
+++ app/src/main/java/com/example/donz/githubtoupload/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "New Toast Added from waleed", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "New Toast Added from Github File from Hamza", Toast.LENGTH_SHORT).show();
     }
 }
